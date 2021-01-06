37eab0a14e34dfc7b71a5183ae95131742548fe7
Rory <rorybr1@gmail.com>
2021-01-06 22:24:22 +0000

    added a map marker + working event listener

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,11 +1,23 @@
-// Intial Settings
- function initMap() {
+ function initMap() { // Initialize Map
     var options = {
-        zoom:10,
+        zoom:14,
         center:{lat:53.2707,lng:-9.0568},
         disableDefaultUI:true
     }
 
     var map = new 
     google.maps.Map(document.getElementById('map'), options);
+
+    // Event Listeners
+    document.getElementById("restaurants").addEventListener("click", showRestaurants);
+
+    // Show Restaurants on #restaurants Click
+    function showRestaurants() {
+        new google.maps.Marker({
+            position: {lat:53.27056881953248, lng:-9.053005040529506},
+            map,
+            title: "Pálás Bar & Kitchen",
+        });
+        }
 }
+
---
+++ assets/js/markers.js
@@ -0,0 +1,9 @@
+let restaurantLocations = [
+    // Pálás Kitchen & Bar
+    {
+    name: {Pálás Kitchen & Bar}
+    coords: { lat: 53.27039967529483, lng: -9.053005040529506 },
+    description: `<h1>`
+    }
+
+]
