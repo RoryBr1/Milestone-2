5ea2d077afe3a4343bfab91761f51ae0bda9adbc
Rory <rorybr1@gmail.com>
2021-01-09 15:51:57 +0000

    moved menu buttons to left of map, added todo div

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,5 @@
 let markers = [];
-
+  
 function initMap() {
   const galway = { lat:53.27108077506178, lng:-9.056759662752283 };
   map = new google.maps.Map(document.getElementById("map"), {
@@ -7,6 +7,47 @@ function initMap() {
     center: galway,
     mapTypeId: "terrain",
   });
+}
+
+const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+    const marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      animation: google.maps.Animation.DROP,
+    });
+    markers.push(marker);
+    
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
+    });
+  }
+
+  
+// Hotels
+const hotelsButton = document.getElementById("hotels");
+hotelsButton.addEventListener("click", showHotels);
+
+function showHotels(){
+    deleteMarkers(); // Clears all previous markers on the map
+    for (let i = 0; i < hotels.length; i++) {
+        addMarker(hotels[i].position)
+    };
+}
+
+// Restaurants
+const restaurantsButton = document.getElementById("restaurants");
+restaurantsButton.addEventListener("click", showRestaurants);
+
+function showRestaurants(){
+    deleteMarkers(); // Clears all previous markers on the map
+    for (let i = 0; i < restaurants.length; i++) {
+        addMarker(restaurants[i].position)
+    };
 }
 
 // Nightlife
@@ -20,13 +61,37 @@ function showNightlife(){
     };
 }
 
-// Adds a marker to the map and push to the array.
-function addMarker(location) {
-  const marker = new google.maps.Marker({
-    position: location,
-    map: map,
-  });
-  markers.push(marker);
+// Historic Sites
+const historicSitesButton = document.getElementById("historic-sites");
+historicSitesButton.addEventListener("click", showHistoricSites);
+
+function showHistoricSites(){
+    deleteMarkers(); // Clears all previous markers on the map
+    for (let i = 0; i < historicSites.length; i++) {
+        addMarker(historicSites[i].position)
+    };
+}
+
+// Nature
+const natureButton = document.getElementById("nature");
+natureButton.addEventListener("click", showNature);
+
+function showNature(){
+    deleteMarkers(); // Clears all previous markers on the map
+    for (let i = 0; i < nature.length; i++) {
+        addMarker(nature[i].position)
+    };
+}
+
+// Activities
+const activitiesButton = document.getElementById("activities");
+activitiesButton.addEventListener("click", showActivities);
+
+function showActivities(){
+    deleteMarkers(); // Clears all previous markers on the map
+    for (let i = 0; i < activities.length; i++) {
+        addMarker(activities[i].position)
+    };
 }
 
 // Sets the map on all markers in the array.
